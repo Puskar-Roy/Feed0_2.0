f5e8162a6888f59fe4c079b15b50b03a3c72f71c
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 17:23:44 +0530

    updated

--- frontend/src/Components/Message/Message.jsx
+++ frontend/src/Components/Message/Message.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { FaPencilAlt } from "react-icons/fa";
 function Message() {
   return (
     <div className="m-10 mt-[8rem] ">
       <div className="grid grid-cols-2 mx-auto shadow-xl mx-auto rounded-lg ">
-        <div className="w-1/3">
-          <div></div>
+        <div className="w-1/3 flex flex-col">
+          <h1 className="flex justify-between">
+            <span>Feed0 Messages</span>
+            <span>
+              <FaPencilAlt />
+            </span>
+          </h1>
         </div>
         <div className="w-2/3">chat section</div>
       </div>
