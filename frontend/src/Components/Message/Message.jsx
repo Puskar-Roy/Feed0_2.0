926560ccd9095ed9ad68075968eed04b859a878f
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 17:29:22 +0530

    updated

--- frontend/src/Components/Message/Message.jsx
+++ frontend/src/Components/Message/Message.jsx
@@ -4,7 +4,7 @@ function Message() {
   return (
     <div className="m-10 mt-[8rem] ">
       <div className="grid grid-cols-2 mx-auto shadow-xl mx-auto rounded-lg ">
-        <div className="w-1/3 flex flex-col">
+        <div className="w-1/3 flex flex-col space-y-4">
           <h1 className="flex justify-between">
             <span>Feed0 Messages</span>
             <span>
