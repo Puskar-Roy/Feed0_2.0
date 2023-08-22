d8d6a9016fd814e371f61e9eec6686a3e058ec4d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 16:53:26 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Notifications/NotificationsPage.jsx
+++ frontend/src/Components/Home/HomeUI/Notifications/NotificationsPage.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBell, FaUser, FaComment } from "react-icons/fa";
 
-const notifications = [
-  {
-    id: 1,
-    text: "Feed0 liked your post.",
-    icon: <FaBell className="text-blue-500" />,
-  },
-  {
-    id: 2,
-    text: "Emily commented on your photo.",
-    icon: <FaComment className="text-green-500" />,
-  },
-  {
-    id: 3,
-    text: "Feed0 Updated to v1.1",
-    icon: <FaUser className="text-purple-500" />,
-  },
-  // Add more notifications here
-];
-
 function NotificationsPage() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    fetchNotificationsFromAPI()
+      .then((data) => {
+        setNotifications(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching notifications:", error);
+      });
+  }, []);
+
+  const fetchNotificationsFromAPI = async () => {
+    // Replace this with API call
+
+    const response = await fetch("backend-api");
+    const data = await response.json();
+    return data;
+  };
+
+  const getNotificationIcon = (type) => {
+    switch (type) {
+      case "like":
+        return <FaBell className="text-blue-500" />;
+      case "comment":
+        return <FaComment className="text-green-500" />;
+      case "update":
+        return <FaUser className="text-purple-500" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="m-12 mt-[6rem]">
-      <div className="bg-white rounded-lg p-6 shadow-md w-full  ">
+      <div className="bg-white rounded-lg p-6 shadow-md w-full">
         <h1 className="text-2xl font-semibold mb-4">Notifications</h1>
         <ul>
           {notifications.map((notification) => (
@@ -31,7 +45,9 @@ function NotificationsPage() {
               key={notification.id}
               className="flex items-center space-x-3 p-2 hover:bg-gray-100 cursor-pointer"
             >
-              <div className="text-2xl">{notification.icon}</div>
+              <div className="text-2xl">
+                {getNotificationIcon(notification.type)}
+              </div>
               <div>
                 <p className="text-gray-700">{notification.text}</p>
               </div>
