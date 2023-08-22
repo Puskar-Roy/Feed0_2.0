1ec4eccbf89842894163d0efbfeea7c49d82286b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 17:21:09 +0530

    updated

--- frontend/src/Components/Message/Message.jsx
+++ frontend/src/Components/Message/Message.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Message() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="m-10 mt-[8rem] ">
+      <div className="grid grid-cols-2 mx-auto shadow-xl mx-auto rounded-lg ">
+        <div className="w-1/3">
+          <div></div>
+        </div>
+        <div className="w-2/3">chat section</div>
+      </div>
     </div>
   );
 }
