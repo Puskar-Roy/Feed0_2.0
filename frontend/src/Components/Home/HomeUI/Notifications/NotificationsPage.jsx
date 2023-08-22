68a23fb90bcff05006235e8a1840156360626d49
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 16:45:57 +0530

    updated

--- frontend/src/Components/Home/HomeUI/Notifications/NotificationsPage.jsx
+++ frontend/src/Components/Home/HomeUI/Notifications/NotificationsPage.jsx
@@ -22,23 +22,21 @@ const notifications = [
 
 function NotificationsPage() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white rounded-lg p-6 shadow-md w-96">
-        <h1 className="text-2xl font-semibold mb-4">Notifications</h1>
-        <ul>
-          {notifications.map((notification) => (
-            <li
-              key={notification.id}
-              className="flex items-center space-x-3 p-2 hover:bg-gray-100 cursor-pointer"
-            >
-              <div className="text-2xl">{notification.icon}</div>
-              <div>
-                <p className="text-gray-700">{notification.text}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="bg-white rounded-lg p-6 shadow-md w-full">
+      <h1 className="text-2xl font-semibold mb-4">Notifications</h1>
+      <ul>
+        {notifications.map((notification) => (
+          <li
+            key={notification.id}
+            className="flex items-center space-x-3 p-2 hover:bg-gray-100 cursor-pointer"
+          >
+            <div className="text-2xl">{notification.icon}</div>
+            <div>
+              <p className="text-gray-700">{notification.text}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
