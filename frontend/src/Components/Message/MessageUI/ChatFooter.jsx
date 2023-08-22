cb6a647a5135842aa25842a0ba46d02a9cc1749d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-08-22 19:34:53 +0530

    updated

--- frontend/src/Components/Message/MessageUI/ChatFooter.jsx
+++ frontend/src/Components/Message/MessageUI/ChatFooter.jsx
@@ -36,21 +36,7 @@ function ChatFooter() {
       >
         <FaSmile />
       </button>
-      {isTyping ? (
-        <button
-          type="button"
-          className="flex flex-shrink-0 focus:outline-none mx-2 block text-blue-600 hover:text-blue-700 w-6 h-6"
-        >
-          <FaPaperPlane />
-        </button>
-      ) : (
-        <button
-          type="button"
-          className="flex flex-shrink-0 focus:outline-none mx-2 block text-red-600 hover:text-red-700 w-6 h-6"
-        >
-          <FaHeart />
-        </button>
-      )}
+
       <div className="relative flex-grow">
         <label>
           <input
@@ -60,14 +46,21 @@ function ChatFooter() {
             onChange={handleInputChange}
             placeholder="Aa"
           />
-          <button
-            type="button"
-            className="absolute top-0 right-0 mt-2 mr-3 flex flex-shrink-0 focus:outline-none block text-blue-600 hover:text-blue-700 w-6 h-6"
-          >
-            <svg viewBox="0 0 20 20" className="w-full h-full fill-current">
-              <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM6.5 9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm7 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm2.16 3a6 6 0 0 1-11.32 0h11.32z" />
-            </svg>
-          </button>
+          {isTyping ? (
+            <button
+              type="button"
+              className=" flex items-center  focus:outline-none mx-2 block text-blue-600 hover:text-blue-700 w-6 h-6"
+            >
+              <FaPaperPlane />
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="flex items-center focus:outline-none mx-2 block text-red-600 hover:text-red-700 w-6 h-6"
+            >
+              <FaHeart />
+            </button>
+          )}
         </label>
       </div>
     </div>
